8915a0620db26a162c22976d109cfe4b0f1fd5f1
piterstany <piotr.babula86@gmail.com>
2023-05-31 18:19:50 +0200

    added simple calculator catalog

---
+++ src/main/java/Exercises/SimpleCalc/SimpleCalc.java
@@ -0,0 +1,6 @@
+package Exercises.SimpleCalc;
+
+public class SimpleCalc {
+//TODO: Make a simple calculator that reads numbers from keyboard and performs the operations: sum, difference, ratio, quotient and modulo.
+
+}
